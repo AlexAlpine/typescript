e6299af9eefb7ab6c6c1f393cf3d2d3b739763c9
AlexAlpine <alex6122223@gmail.com>
2024-09-27 17:33:45 +0200

    lesson11

---
+++ src/lessons/lesson_10/Lesson10.tsx
@@ -0,0 +1,11 @@
+// import styles from './lesson10.module.css'
+
+const Lesson10 = () => {
+return (
+<div className='lesson-container'>
+  <h2>Самостоятельная работа</h2>
+</div>
+)
+}
+
+export default Lesson10;
---
+++ src/lessons/lesson_11/Lesson11.tsx
@@ -0,0 +1,11 @@
+
+const Lesson11 = () => {
+  return (
+    <div className='lesson-container'>
+      <h2>Lesson 11</h2>
+      <p>На этом уроке мы настраивали маршрутизацию из библиотеки react-router-dom</p>
+    </div>
+  );
+};
+
+export default Lesson11;
